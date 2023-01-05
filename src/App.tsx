804d4a06a4a94d8a93b4d7fc51c060689f4fcacc
ooooorobo <orobos654@gmail.com>
2023-01-05 23:12:32 +0900

    chore: add eslint& prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import Button from '@mui/material/Button';
+import React from "react";
+import Button from "@mui/material/Button";
 
 function App() {
   return (
     <div className="App">
       hi
-    <Button variant="contained">Hello World</Button>
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 }
