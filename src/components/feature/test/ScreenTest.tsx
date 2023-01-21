c02febd826c6532e117fbc0b6aff8d0595ec0079
조예진 <orobos654@gmail.com>
2023-01-21 16:39:08 +0900

    feat: add video wrapper to remove blink

--- src/components/feature/test/ScreenTest.tsx
+++ src/components/feature/test/ScreenTest.tsx
@@ -3,6 +3,7 @@ import useMediaRecorder, { RecordingStatus } from "../../../hooks/useMediaRecord
 import { Button } from "@mui/material";
 import { useEffect, useState } from "react";
 import StreamVideo from "../../common/StreamVideo";
+import styled from "@emotion/styled";
 
 export default function ScreenTest() {
   const { status, mediaStream, startRecord, stopRecord, recordedBlob } = useMediaRecorder(true);
@@ -19,21 +20,29 @@ export default function ScreenTest() {
 
   return (
     <>
-      {showStream ? (
+      <VideoWrapper>
         <StreamVideo stream={mediaStream} width={700} height={400} />
-      ) : (
-        <RecordedVideo
-          src={recordedBlob}
-          width={700}
-          height={400}
-          onEnded={() => {
-            setShowStream(true);
-          }}
-        />
-      )}
+        {!showStream && (
+          <RecordedVideo
+            src={recordedBlob}
+            width={700}
+            height={400}
+            onEnded={() => {
+              setShowStream(true);
+            }}
+          />
+        )}
+      </VideoWrapper>
       <Button onClick={status === RecordingStatus.RECORDING ? stopRecord : startRecord}>
         {status === RecordingStatus.RECORDING ? "녹화 중단" : "녹화 시작"}
       </Button>
     </>
   );
 }
+
+const VideoWrapper = styled.div`
+  position: relative;
+  video {
+    position: absolute;
+  }
+`;
