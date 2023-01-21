1991e23eae6f63472fd45d3e0476f49eb33af60d
조예진 <orobos654@gmail.com>
2023-01-21 15:13:23 +0900

    chore: adjust eslint

--- src/hooks/useMediaRecorder.ts
+++ src/hooks/useMediaRecorder.ts
@@ -1,48 +1,51 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 export default function useMediaRecorder(initOnCall = false) {
-    const [mediaStream, setMediaStream] = useState<MediaStream>();
-    const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder>();
-    const [recordedBlob, setRecordedBlob] = useState<string>()
+  const [mediaStream, setMediaStream] = useState<MediaStream>();
+  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder>();
+  const [recordedBlob, setRecordedBlob] = useState<string>();
 
-    const initMediaStream = async () => {
-        const mediaStream = await navigator.mediaDevices.getUserMedia({
-            audio: true,
-            video: true,
-        });
-        setMediaStream(mediaStream);
-        const mediaRecorder = new MediaRecorder(mediaStream, {mimeType: "video/webm"});
-        mediaRecorder.ondataavailable = (e) => {
-            console.log(e.data)
-            const url = window.URL.createObjectURL(e.data);
-            setRecordedBlob(url)
-            console.log(url)
-        };
-        setMediaRecorder(mediaRecorder);
+  const initMediaStream = async () => {
+    const mediaStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    setMediaStream(mediaStream);
+
+    const mediaRecorder = new MediaRecorder(mediaStream, { mimeType: "video/webm" });
+    setMediaRecorder(mediaRecorder);
+
+    mediaRecorder.ondataavailable = (e) => {
+      const url = window.URL.createObjectURL(e.data);
+      setRecordedBlob(url);
+    };
+  };
+
+  useEffect(() => {
+    if (initOnCall) {
+      initMediaStream();
+    }
+  }, []);
+
+  const startRecord = async () => {
+    if (!mediaRecorder) {
+      await initMediaStream();
     }
 
-    useEffect(() => {
-        if (initOnCall) {
-            initMediaStream();
-        }
-    }, [])
+    mediaRecorder?.start();
+  };
 
-    const startRecord = async () => {
-        if (!mediaRecorder) {
-            await initMediaStream();
-        }
-
-        mediaRecorder?.start();
+  const stopRecord = () => {
+    if (!mediaRecorder) {
+      throw new Error("stream not found");
     }
+    mediaRecorder.stop();
+  };
 
-    const stopRecord = () => {
-        if (!mediaRecorder) {
-            throw new Error('stream not found')
-        }
-        mediaRecorder.stop();
-    }
-
-    return {
-        mediaStream, startRecord, stopRecord, recordedBlob
-    }
+  return {
+    mediaStream,
+    startRecord,
+    stopRecord,
+    recordedBlob,
+  };
 }
